70e0c5cc97ebf16008c8b0afbdfac1bbb4603481
Alexander Kostylev <successful08@mail.ru>
2024-01-27 23:50:27 +0500

    sem 1 commit

---
+++ src/clients/Dog.java
@@ -0,0 +1,14 @@
+package clients;
+
+import java.time.LocalDate;
+
+public class Dog extends Animal {
+
+    public Dog(String nickName, Owner owner, LocalDate birthDate, Illness illness) {
+        super(nickName, owner, birthDate, illness); //Сходи в род.класс и вызови 4 параметра
+    }
+
+    public Dog(){
+        super();
+    }
+}
